c06432ee8a005d2f26929c58fd2645d818fdb79a
Kiryl Alsheuski <kirill10olsh@gmail.com>
2025-09-24 20:59:34 +0200

    Round seekbar + vinyl

--- music-player/src/js/player.js
+++ music-player/src/js/player.js
@@ -8,20 +8,25 @@ const progressCircle = document.getElementById('progressCircle');
 const progressText = document.getElementById('progressText');
 const progressSlider = document.getElementById('progressSlider');
 
+
 volumeControl.addEventListener('input', (e) => {
     player.audio.volume = e.target.value;
 });
 
 class MusicPlayer {
     constructor(tracks) {
+        this.vinyl_angle = 0;
+        this.change_amount = 11;
         this.tracks = tracks;
         this.currentTrackIndex = 0;
         this.audio = new Audio();
         this.isPlaying = false;
         this.isMoving = false;
+        this.last_angle = 0;
 
         this.audio = document.getElementById('audio');
-        //this.seekbar = document.getElementById('seekbar');
+        this.seekbar = document.getElementById('progressCircle');
+        this.vinyl = document.getElementsByClassName('track-art')[0];
         //this.seekbar_cont = document.getElementsByClassName('seekbar-container');
 
         // Initialize seekbar
@@ -29,14 +34,23 @@ class MusicPlayer {
             if (this.isMoving) return;
             else {
                 const percentage = (this.audio.currentTime / this.audio.duration);
-                console.log(percentage*100);
-                //this.seekbar.style.height = (percentage * 100) + "%";
+                console.log(percentage * 100);
+                this.vinyl_angle += 5;
+                if (this.vinyl_angle >= 360) {
+                    this.vinyl_angle -= 360;
+                    this.vinyl.style.transition = "all 0s";
+                }
+                else {
+                    this.vinyl.style.transition = "all 0.5s";
+                }
+                console.log(this.vinyl_angle);
+                this.vinyl.style.rotate = this.vinyl_angle + "deg";
                 setProgress(percentage * 100);
             }
         });
 
         // Handle seekbar click/drag
-        //this.seekbar.addEventListener('click', (e) => this.seek(e));
+        this.seekbar.addEventListener('mousedown', (e) => this.click(e));
         //this.seekbar_cont[0].addEventListener('mousedown', (e) => this.seek(e));
         //this.seekbar_cont[0].addEventListener('click', (e) => this.seek(e));
         // this.seekbar_cont[0].addEventListener('mousedown', (e) => this.click(e));
@@ -44,44 +58,115 @@ class MusicPlayer {
 
     }
 
-    // click(event) {
-    //     this.isMoving = true;
-    //     this.seekbar_cont[0].addEventListener('mousemove', (e) => this.changeSeek(e));
-    //     this.seekbar_cont[0].addEventListener('mouseup', (e) => this.mouseup(e));
-    // }
-
-    // mouseup(event) {
-    //     console.log("Mouse up");
-    //     this.isMoving = false;
-    //     const percent = event.offsetY / this.seekbar_cont[0].offsetHeight;
-    //     this.audio.currentTime = percent * this.audio.duration;
-
-    //     console.log("Set current time to " + this.audio.currentTime);
-    //     this.seekbar_cont[0].removeEventListener('mousemove', (e) => this.changeSeek(e));
-    //     this.seekbar_cont[0].removeEventListener('mouseup', (e) => this.mouseup(e))
-    //     console.log(this.seekbar_cont[0].removeEventListener('mouseup', (e) => this.mouseup(e)));
-    // }
-
-    seek(event) {
-        const percent = event.offsetY / this.seekbar_cont[0].offsetHeight;
-        this.audio.currentTime = percent * this.audio.duration;
-        setProgress(Math.round(percent * 100));
-        // this.seekbar.style.height = (percent * 100) + "%";
+    click(event) {
+        if (event.button == 2) {
+            console.log("🖱 right click detected!")
+            
+            this.isMoving = true;
+            this.seekbar.addEventListener('mousemove', (e) => this.changeSeek_DJ(e));
+            this.seekbar.addEventListener('mouseup', (e) => this.mouseup(e));
+        }
+        if (event.button == 0) {
+            console.log("🖱 left click detected!")
+            this.isMoving = true;
+            this.seekbar.addEventListener('mousemove', (e) => this.changeSeek(e));
+            this.seekbar.addEventListener('mouseup', (e) => this.mouseup(e));
+        }
     }
 
-    // changeSeek(event) {
-    //     if (!this.isMoving) {
-    //         return;
-    //     }
-    //     else {
-    //         const percent = event.offsetY / this.seekbar_cont[0].offsetHeight;
-    //         console.log(event.offsetX);
-    //         console.log(percent);
+    mouseup(event) {
+        console.log("Mouse up");
+        this.isMoving = false;
+        const X = event.offsetX - 100;
+        const Y = (event.offsetY - 100) * (-1);
+        let angle = Math.atan2(X, Y) * (180 / Math.PI);
 
-    //         //this.audio.currentTime = percent * this.audio.duration;
-    //         this.seekbar.style.height = (percent * 100) + "%";
+        if (angle < 0) {
+            angle = 360 + angle;
+        }
+        
+        console.log(angle);
+        setProgressAngle(angle);
+        let percent = angle / 360;
+        console.log("Percent: " + percent);
+        this.audio.currentTime = percent * this.audio.duration;
+
+        console.log("Set current time to " + this.audio.currentTime);
+        this.seekbar.removeEventListener('mousemove', (e) => this.changeSeek(e));
+        this.seekbar.removeEventListener('mouseup', (e) => this.mouseup(e))
+        console.log(this.seekbar.removeEventListener('mouseup', (e) => this.mouseup(e)));
+        audioElement.playbackRate=1.0;
+        this.change_amount = 11;
+    }
+
+    // seek(event) {
+    //     const X = event.offsetX - 100;
+    //     const Y = (event.offsetY - 100) * (-1);
+    //     let angle = Math.atan2(X, Y) * (180 / Math.PI);
+
+    //     if (angle < 0) {
+    //         angle = 360 + angle;
     //     }
+
+    //     console.log(angle);
+    //     setProgressAngle(angle);
     // }
+
+    changeSeek(event) {
+        if (!this.isMoving) {
+            return;
+        }
+        else {
+            const X = event.offsetX - 100;
+            const Y = (event.offsetY - 100) * (-1);
+            let angle = Math.atan2(X, Y) * (180 / Math.PI);
+            
+            if (angle < 0) {
+                angle = 360 + angle;
+            }
+
+            this.vinyl_angle += angle - this.last_angle;
+            //console.log("Vinyl: " + this.vinyl_angle + " Last: " + this.last_angle + " Angle: " + angle);
+            this.last_angle = angle;
+            this.vinyl.style.transition = "all 0.1s";
+            this.vinyl.style.rotate = this.vinyl_angle + "deg";
+            setProgressAngle(angle);
+        }
+    }
+    
+    changeSeek_DJ(event) {
+        if (!this.isMoving) {
+            return;
+        }
+        else {
+            const X = event.offsetX - 100;
+            const Y = (event.offsetY - 100) * (-1);
+            let angle = Math.atan2(X, Y) * (180 / Math.PI);
+            let change;
+
+            if (angle < 0) {
+                angle = 360 + angle;
+            }
+            
+            change = this.last_angle - angle;
+            if (change < 0) {
+                change = change * (-1);
+
+            }
+            if (this.change_amount > 10) {
+                audioElement.playbackRate=0.5;
+                this.change_amount = 0;
+            }
+            this.change_amount += 1;
+            console.log("Change: " + change);
+            this.vinyl_angle += angle - this.last_angle;
+            console.log("Vinyl: " + this.vinyl_angle + " Last: " + this.last_angle + " Angle: " + angle);
+            this.last_angle = angle;
+            this.vinyl.style.transition = "all 0.1s";
+            this.vinyl.style.rotate = this.vinyl_angle + "deg";
+            setProgressAngle(angle);
+        }
+    }
 
     loadTrack(index) {
         this.audio.src = this.tracks[index];
@@ -96,7 +181,6 @@ class MusicPlayer {
             this.isPlaying = true;
             playButton.classList.remove('fa-play');
             playButton.classList.add('fa-pause');
-            setProgress(10);
         }
         else {
             // Pause the track
@@ -107,17 +191,16 @@ class MusicPlayer {
         }
     }
 
-    pause() {
-        this.seekbar_cont[0].removeEventListener('mousemove', (e) => this.changeSeek(e));
-    }
-
     next() {
         // Move to the next track
         this.currentTrackIndex = (this.currentTrackIndex + 1) % this.tracks.length;
         this.loadTrack(this.currentTrackIndex);
         this.isPlaying = !this.isPlaying;
         this.play();
-        this.seekbar.value = 0;
+        setProgressAngle(0);
+        this.vinyl_angle = 0;
+        this.vinyl.style.transition = "all 0.5s";
+        this.vinyl.style.rotate = this.vinyl_angle + "deg";
     }
 
     previous() {
@@ -126,7 +209,10 @@ class MusicPlayer {
         this.loadTrack(this.currentTrackIndex);
         this.isPlaying = !this.isPlaying;
         this.play();
-        this.seekbar.value = 0;
+        setProgressAngle(0);
+        this.vinyl_angle = 0;
+        this.vinyl.style.transition = "all 0.5s";
+        this.vinyl.style.rotate = this.vinyl_angle + "deg";
     }
 
     getCurrentTrack() {
@@ -163,12 +249,15 @@ function setProgress(percentage) {
                 #72f2cc ${angle}deg,
                 #b3b3b3 ${angle}deg
             )`;
+}
 
-    // Update the text
-    progressText.textContent = `${Math.round(percentage)}%`;
-
-    // Update slider
-    progressSlider.value = percentage;
+function setProgressAngle(angle) {
+    // Update the conic gradient
+    progressCircle.style.background = `conic-gradient(
+                #72f2cc 0deg,
+                #72f2cc ${angle}deg,
+                #b3b3b3 ${angle}deg
+            )`;
 }
 
 function animateProgress() {
